f75206a62b898e21bd37a47abef9abe7f1c4d5d3
kaushal <kaushal_chakka@srmap.edu.in>
2026-06-20 13:01:20 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let personal_items=["Toothbrush","Toothpaste","Comb","Soap","Shampoo","Perfume","Sunscreen"];
+let electronics=["Phone","Charger","Earphones","Power Bank","Watch"];
+let utility=["Wallet","Government Id","Cash","Tickets"];
+let health=["Pain relivers","Vitamins","Fever tablets"];
+let clothes=["2 Shirts","Pants","Shorts","Dress","Innerwear"];
+let accesory=["Sunglass","Hair tie","Jewels","Hat"];
+let swim=["Swimsuit","Beach bag","Beach towel"];
+let footwear=["Sandals","Shoes","Flip-Flops"];
+
